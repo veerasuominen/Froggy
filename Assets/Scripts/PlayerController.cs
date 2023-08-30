1987f4b232a8adcb23b6b0ec30410d1b411d6b15
Veera Suominen <veera.suomine@gmail.com>
2023-08-30 13:49:58 +0300

    ui stuff, dashing etc etc

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -12,53 +14,95 @@ public class PlayerController : MonoBehaviour
     public float sprintSpeed = 2;
     public bool sprinting;
 
+    //health
+    public int maxHealth = 5;
+    public int currentHeath;
+    public Health healthBar;
+
+    //dashing
+    public bool canDash;
+    public bool isDashing;
+    private Vector2 dashingDirection;
+    private float dashCooldown = 2;
+    private float dashingVelocity = 5;
+    public TrailRenderer tr;
+
     public GameObject tongue;
     public Collider2D rangeCollider;
-
     public bool isInRange;
     public float tongueSpeed = 10;
-
     public bool arrowPressed;
-
     public Vector2 playerPosition;
     public Vector2 mousePosition;
 
     // Start is called before the first frame update
     private void Start()
     {
+        currentHeath = maxHealth;
+        healthBar.SetMaxHealth(maxHealth);
     }
 
     // Update is called once per frame
     private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.F))
+        {
+            TakeDamage(1);
+        }
+        //if (Input.GetKeyDown(KeyCode.Space) && canDash)
+        //{
+        //    isDashing = true;
+        //    canDash = false;
+        //    tr.emitting = true;
+        //    dashingDirection = new Vector2(movement.x, movement.y);
+        //    if (dashingDirection == Vector2.zero)
+        //    {
+        //        dashingDirection = new Vector2(transform.localScale.x, 0f);
+        //    }
+        //}
+
         movement.y = Input.GetAxis("Vertical");
         movement.x = Input.GetAxis("Horizontal");
+
         Movement();
         Rotation();
+
         //TongueGrapple();
 
         playerPosition = transform.position;
     }
 
-    //private void TongueGrapple()
-    //{
-    //    if (Input.GetKeyDown(KeyCode.Mouse0))
-    //    {
-    //        arrowPressed = true;
-    //        tongue.transform.Translate(0, tongueSpeed * Time.deltaTime, 0);
-    //    }
+    private void TakeDamage(int damage)
+    {
+        currentHeath -= damage;
+        healthBar.SetHealth(currentHeath);
+    }
 
+    //public IEnumerator Dash()
+    //{
+    //    canDash = false;
+    //    isDashing = true;
+    //    float originalGravity = rb.gravityScale;
+    //    rb.gravityScale = 0f;
+    //    rb.velocity = new Vector2(transform.localScale.x * dashingPower, 0f);
+    //    tr.emitting = true;
+    //    yield return new WaitForSeconds(dashingTime);
+    //    tr.emitting = false;
+    //    rb.gravityScale = originalGravity;
+    //    isDashing = false;
+
+    //    yield return new WaitForSeconds(dashCooldown);
+    //    canDash = true;
     //}
 
     private void Rotation()
     {
-        if (Input.GetKey(KeyCode.A))
+        if (Input.GetKeyDown(KeyCode.A) && transform.rotation != null)
         {
-            playerSprite.flipX = false;
+            transform.SetPositionAndRotation(transform.position, transform.rotation);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            playerSprite.flipX = true;
         }
     }
 
---
+++ Assets/Scripts/UI/Health.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Health : MonoBehaviour
+{
+    public Slider slider;
+
+    public void SetMaxHealth(int health)
+    {
+        slider.maxValue = health;
+        slider.value = health;
+    }
+
+    public void SetHealth(int health)
+    {
+        slider.value = health;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
